b50207eb804736c62d690ad805f8a416b1c7d2f6
Romain HENRY <romainhenry@MacBook-Pro-de-Romain.local>
2022-01-06 22:17:45 +0100

    add reducer for login

---
+++ src/code/Login/index.tsx
@@ -0,0 +1,60 @@
+import styled from 'styled-components'
+import { Spinner } from '../Loader';
+import axios from 'axios';
+import { useNavigate } from 'react-router';
+
+type LoginInitAction = {
+    type: 'LOGIN_INIT';
+}
+
+type LoginSucessAction = {
+    type: 'LOGIN_SUCCESS';
+    payload: string;
+}
+
+type LoginFailureAction = {
+    type: 'LOGIN_FAILURE';
+}
+
+type LoginState = {
+    data: string | undefined;
+    isLoading:  boolean;
+    isError: boolean;
+}
+
+type LoginAction =
+    | LoginInitAction
+    | LoginSucessAction
+    | LoginFailureAction
+
+const loginReducer = (state: LoginState, action: LoginAction) => {
+    switch (action.type) {
+        case 'LOGIN_INIT':
+            return {
+                ...state,
+                isLoading: true,
+                isError: false
+            }
+        case 'LOGIN_SUCCESS':
+            return {
+                ...state,
+                isLoading: false,
+                isError: false,
+                data: action.payload
+            }
+        case 'LOGIN_FAILURE':
+            return {
+                ...state,
+                isLoading: false,
+                isError : true
+            }
+        default:
+            return state
+    }
+}
+
+export const Login = () => {
+    return (
+        <div>Login</div>
+    )
+};
